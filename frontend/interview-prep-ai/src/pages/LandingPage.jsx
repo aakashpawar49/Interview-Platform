c7fda17e8ab66c384f552dfdfd4aedb82bd240d6
Aakash <aakashpawar496@gmail.com>
2025-07-01 08:54:25 +0530

    Landing UI Completed

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import HERO_IMG from "../assets/hero-img.png";
 import { APP_FEATURES } from "../utils/data";
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { LuSparkles } from "react-icons/lu";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -43,7 +44,7 @@ const LandingPage = () => {
               <h1 className='text-5xl text-black font-medium mb-6 leading-tight'>
                 Ace Interviews with <br />
                 <span className='text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,_#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold'>
-                  AI-Powered
+                  <LuSparkles />AI-Powered
                 </span>{" "}
                 Learning
               </h1>
@@ -68,6 +69,81 @@ const LandingPage = () => {
         </div>
       </div>
       
+      <div className='w-full min-h-full relative z-10'>
+        <div>
+          <section className='flex items-center justify-center -mt-36'>
+            <img
+              src={HERO_IMG}  
+              alt="HERO Image"
+              className="w-[80vw] rounded-lg"
+            />
+          </section>
+        </div>
+
+        <div className='w-full min-h-full bg-[#FFFCEF] mt-10'>
+          <div className='container mx-auto px-4 pt-10 pb-20'>
+            <section className='mt-5'>
+              <h2 className='text-2xl font-medium text-center mb-12'>
+                Features That Make You Shine
+              </h2>
+
+              <div className='flex flex-col items-center gap-8'>
+                {/* First 3 cards */}
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-8 w-full'>
+                  {APP_FEATURES.slice(0, 3).map((feature) => {
+                    <div
+                      key={feature.id}
+                      className='bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transform-border border-amber-100'
+                    >
+                      <h3 className='text-base font-semibold mb-3'>
+                      {feature.title}
+                      </h3>
+                      <p className='text-gray-600'>{feature.description}</p>
+                    </div>
+                  })}
+                </div>
+
+                {/* Remaining 2 Cards */}
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+                  {APP_FEATURES.slice(3).map((feature) => (
+                    <div
+                      key={feature.id}
+                      className='bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transition border border-amber-100'
+                    >
+                      <h3 className='text-base font-semibold mb-3'>
+                        {feature.title}
+                      </h3>
+                      <p className='text-gray-600'>{feature.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+
+        <div className='text-sm bg-gray-50 text-secondary text-center p-5 mt-5'>
+          Made with ❤... Happy Coding
+        </div>
+      </div>
+
+    <Modal 
+      isOpen={openAuthModal}
+      onClose={() => {
+        setOpenAuthModal(false);
+        setCurrentPage("login");
+      }}
+      hideHeader
+      >
+        <div>
+          {currentPage === "login" && (
+            <Login setCurrentPage={setCurrentPage} />
+          )}
+          {currentPage === "signup" && (
+            <SignUp setCurrentPage={setCurrentPage} />
+          )}
+        </div>
+        </Modal>
     </>
   );
 };
