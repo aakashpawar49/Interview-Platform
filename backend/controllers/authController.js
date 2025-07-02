6dc0578f311edee1280378a46961ad469ca54948
Aakash <aakashpawar496@gmail.com>
2025-07-02 11:30:17 +0530

    Hehe

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,25 +4,28 @@ const jwt = require("jsonwebtoken");
 
 //Generate JWT Token
 const generateToken = (userId) => {
-    return jwt.sign({ id: UserId}, process.env.JWT_SECRET, { expiresIn: "7d" });
+    return jwt.sign({ id: userId}, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 // @desc    Register a new user
 // @route   POST /api/auth/register
 // @access  Public
 const registerUser = async (req, res) => {
+    res.send("Register route works");
 };
 
 // @desc    Login user
 // @route   POST /api/auth/login
 // @access  Public
 const loginUser = async (req, res) => {
+    res.send("Login route works");
 };
 
 // @desc    Get user profile
 // @route   POST /api/auth/profile
 // @access  Public
 const getUserProfile = async (req, res) => {
+    res.send("Profile route works");
 };
 
 module.exports = { registerUser, loginUser, getUserProfile };
