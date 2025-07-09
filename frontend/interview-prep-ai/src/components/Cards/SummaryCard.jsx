323fc195606e4fe3052b0088fad03628fa1493f3
Aakash <aakashpawar496@gmail.com>
2025-07-10 00:25:05 +0530

    Summary Card

--- frontend/interview-prep-ai/src/components/Cards/SummaryCard.jsx
+++ frontend/interview-prep-ai/src/components/Cards/SummaryCard.jsx
@@ -12,65 +12,67 @@ const SummaryCard = ({
     onDelete
 }) => {
   return <div
-    className=''
-    onClick={onSelect}
+        className='bg-white border border-gray-300/40 rounded-xl overflow-hidden cursor-pointer hover:shadow-xl shadow-gray-100 relative group'
+        onClick={onSelect}
     >
     <div
-      className=''
+      className='rounded-lg cursor-pointer relative'
       style={{
         background: colors.bgcolor,
       }}
     >
-        <div className=''>
-            <div className=''>
-                <span className=''>
+        <div className='flex items-start'>
+            <div className='flex-shrink-0 w-12 h-12 rounded-md flex items-center justify-center mr-4'>
+                <span className='text-lg font-semibold text-black'>
                     GU
                 </span>
             </div>
 
             {/* Content  Container */}
-            <div className=''>
-                <div className=''>
+            <div className='flex-grow'>
+                <div className='flex justify-between items-start'>
                     {/* Title and Skills */}
+                    <div>
+                        <h2 className='text-[17px] font-medium'>{role}</h2>
+                        <p className='text-xs text-medium text-gray-900'>
+                        {topicsToFocus}
+                        </p>
+                    </div>
                 </div>
-                <h2 className=''>{role}</h2>
-                <p className=''>
-                    {topicsToFocus}
-                </p>
             </div>
         </div>
-    </div>
 
-    <button
-        className=''
-        onClick={(e) => {
-            e.stopPropagation();
-            onDelete();
-        }}
-    >
-        delete
-    </button>
+        <button
+            className='hidden group-hover:flex items-center gap-2 text-xs text-rose-500 font-medium bg-rose-50 px-3 py-1 rounded text-nowrap border border-rose-100 hover:border-rose-200 cursor-pointer absolute top-0 right-0'
+            onClick={(e) => {
+                e.stopPropagation();
+                onDelete();
+            }}
+        >
+            delete
+        </button>
+       </div>
 
-    <div className=''>
-        <div className=''>
-            <div className=''>
+    <div className='px-3 pb-3'>
+        <div className='flex items-center gap-3 mt-4'>
+            <div className='text-[10px] font-medium text-black px-3 py-1 border-[0.5px] border-gray-900 rounded-full '>
                 Experience: {experience} {experience == 1 ? "Year" : "Years"}
             </div>
 
-            <div className=''>
+            <div className='text-[10px] font-medium text-black px-3 py-1 border-[0.5px] border-gray-900 rounded-full '>
                 {question} Q&A 
             </div>
 
-            <div className=''>
+            <div className='text-[10px] font-medium text-black px-3 py-1 border-[0.5px] border-gray-900 rounded-full '>
                 Last Updated: {lastUpdated}
             </div>
         </div>
 
         {/* Description */}
-        <p className=''>
+        <p className='text-[12px] text-gray-500 font-medium line-clamp-2 mt-3'>
             {description}
         </p>
-    </div>
+       </div>
     </div>
 }
 
