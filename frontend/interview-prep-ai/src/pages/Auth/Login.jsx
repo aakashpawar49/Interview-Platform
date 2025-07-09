30b6e124381552d6da4d5bfcaa2abcabc49db92e
Aakash <aakashpawar496@gmail.com>
2025-07-09 10:12:06 +0530

    UserContext created

--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
+import { userContext } from '../../context/userContext';
 
 const Login = ({ setCurrentPage }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const { updateUser } = useContext(userContext);
 
   const navigate = useNavigate();
 
@@ -37,12 +40,13 @@ const Login = ({ setCurrentPage }) => {
 
       const { token } = response.data;
 
-      if(token) {
+      if (token) {
         localStorage.setItem("token", token);
+        updateUser(response.data);
         navigate("/dashboard");
       }
     } catch (error) {
-      if(error.response && error.response.data.message) {
+      if (error.response && error.response.data.message) {
         setError(error.response.data.message);
       } else {
         setError("Something went wrong. Please try again.");
