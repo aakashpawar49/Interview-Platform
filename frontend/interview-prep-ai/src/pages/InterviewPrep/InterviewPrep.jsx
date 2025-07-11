5d2c0269cc5a3b39fab9d33b1573e0ad3f933ff7
Aakash <aakashpawar496@gmail.com>
2025-07-11 22:22:59 +0530

    Completed

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
 import { AnimatePresence, motion } from "framer-motion";
-import { LuCircleAlert } from 'react-icons/lu';
+import { LuCircleAlert, LuListCollapse } from 'react-icons/lu';
 import SpinnerLoader from '../../components/Loader/SpinnerLoader';
 import { toast } from "react-hot-toast";
 import RoleInfoHeader from './components/RoleInfoHeader';
@@ -44,8 +44,6 @@ const InterviewPrep = () => {
   //Fetch session data by session id
   const toggleQuestionExplanation = async (questionId) => {};
 
-  // Add more questions to a session
-  const uploadMoreQuestions = async () => {};
 
   //Generate Concept Explanation
   const generateConceptExplanation = async (question) => {
@@ -90,6 +88,48 @@ const InterviewPrep = () => {
       }
     } catch (error) {
       console.error("Error:", error);
+    }
+  };
+
+  // Add more questions to a session
+  const uploadMoreQuestions = async () => {
+    try {
+      setIsUpdateLoader(true);
+
+      // Call AI API to generate questions
+      const aiResponse = await axiosInstance.post(
+        API_PATHS.AI.GENERATE_QUESTIONS,
+        {
+            role: sessionData?.role,
+            experience: sessionData?.experience,
+            topicsToFocus: sessionData?.topicsToFocus,
+            numberOfQuestions: 10,
+        }
+      );
+
+      // Should be array like [{question, answer}, ....]
+      const generatedQuestions = aiResponse.data;
+      
+      const response = await axiosInstance.post(
+        API_PATHS.QUESTION.ADD_TO_SESSION,
+        {
+          sessionId,
+          questions: generatedQuestions,
+        }
+      );
+
+      if (response.data) {
+        toast.success("Added More Q&A!!");
+        fetchSessionDetailsById();
+      }
+    } catch (error) {
+      if(error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    } finally {
+      setIsUpdateLoader(false);
     }
   };
 
@@ -152,6 +192,24 @@ const InterviewPrep = () => {
                         isPinned={data?.isPinned}
                         onTogglePin={() => toggleQuestionPinStatus(data._id)}
                       />
+
+                    {!isLoading && 
+                        sessionData?.questions?.length == index + 1 && (
+                          <div className="flex items-center justify-center mt-5">
+                            <button
+                              className='flex items-center gap-3 text-sm text-white font-medium bg-black px-5 py-2 mr-2 rounded text-nowrap cursor-pointer'
+                              disabled={isLoading || isUpdateLoader}
+                              onClick={uploadMoreQuestions}
+                            >
+                              {isUpdateLoader ? (
+                                <SpinnerLoader />
+                              ) : (
+                                <LuListCollapse className='text-lg' />
+                              )}{" "}
+                              Load More
+                            </button>
+                          </div>
+                      )}
                     </>
                   </motion.div>
                 );
