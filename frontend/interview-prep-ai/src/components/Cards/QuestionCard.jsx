db235ec1124e6d130d3c30463f2426fc5e12bbf1
Aakash <aakashpawar496@gmail.com>
2025-07-11 08:24:21 +0530

    QuestionCards done

--- frontend/interview-prep-ai/src/components/Cards/QuestionCard.jsx
+++ frontend/interview-prep-ai/src/components/Cards/QuestionCard.jsx
@@ -25,52 +25,52 @@ const QuestionCard = ({
         setIsExpanded(!isExpanded);
     };
   return <>
-        <div className=''>
-            <div className=''>
-                <div className=''>
-                    <span className=''>
+        <div className='bg-white rounded-lgmb-4 overflow-hidden py-4 px-5 shadow-xl shadows-gray-100/70 border border-gray-100/60 group'>
+            <div className='flex items-start justify-between cursor-pointer'>
+                <div className='flex items-start gap-3.5'>
+                    <span className='text-xs md:text-[15px] font-semibold text-gray-400 leading-[18px]'>
                         Q
                     </span>
 
                     <h3
-                      className=''
+                      className='text-xs md:text-[14px] font-medium text-gray-800 mr-0 md:mr-20'
                       onClick={toggleExpand}
                     >
                         {question}
                     </h3>
                 </div>
 
-                <div className=''>
+                <div className='flex items-center justify-end ml-4 relative'>
                     <div
                         className={`flex ${
                             isExpanded ? "md:flex" : "md:hidden group-hover:flex"
                         }`}
                     >
                         <button
-                            className=''
+                            className='flex items-center gap-2 text-xs text-indigo-800 font-medium bg-indigo-50 py-3 px-1 mr-2 rounded text-nowrap border border-indigo-50 hover:border-indigo-200 cursor-pointer '
                             onClick={onTogglePin}
                         >
                             {isPinned ? (
-                                <LuPinOff className='' />
+                                <LuPinOff className='text-xs' />
                             ) : (
-                                <LuPin className='' />
+                                <LuPin className='text-xs' />
                             )}
                         </button>
 
                         <button
-                            className=''
+                            className='flex items-center gap-2 text-xs text-cyan-800 font-mediumm bg-cyan-50 px-3 py-1 rounded text-nowrap broder border-cyan-50 hover:border-cyan-200 cursor-pointer'
                             onClick={() => {
                                 setIsExpanded(true);
                                 onLearnMore();
                             }}
                         >
                             <LuSparkles />
-                            <span className=''>Learn More</span>
+                            <span className='hidden md:block'>Learn More</span>
                         </button>
                     </div>
 
                     <button
-                        className=''
+                        className='text-gray-400 hover:text-gray-500 cursor-pointer'
                         onClick={toggleExpand}
                     >
                         <LuChevronDown
@@ -84,12 +84,12 @@ const QuestionCard = ({
             </div>
 
             <div
-                className=''
+                className='overflow-hidden transition-all duration-300 easin-out'
                 style={{ maxHeight: `${height}px` }}
             >
             <div
                 ref={contentRef}
-                className=''
+                className='mt-4 text-gray-700 bg-gray-50 px-5 py-3 rounded-lg'
                 >            
             </div>
            </div>
